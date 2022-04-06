b9c806206207185606cd210b0e132d48b8ba3597
Clemens Wältken <clemens.waltken@microsoft.com>
2022-04-06 11:43:08 +0200

    Fix BASE_PATH and node version

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.BASE_URL ? `/${process.env.BASE_URL}` : "/",
+  base: process.env.BASE_PATH ? `/${process.env.BASE_PATH}` : "/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
