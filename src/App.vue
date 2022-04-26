914c613e5ec2a7cde1cc3d142875a0308791f671
socullen <socullen@microsoft.com>
2022-04-26 11:00:49 +0200

    Update nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import WelcomeKitchenSink from '@/components/WelcomeKitchenSink.vue'
-import Navbar from "@/components/Navbar.vue"
+import { RouterView } from "vue-router";
+import WelcomeKitchenSink from "@/components/WelcomeKitchenSink.vue";
+import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-  <header>
-    <Navbar />
-
-  </header>
-  <main class="container auto-mx">
-    <div class="wrapper">
+  <div class="drawer drawer-mobile">
+    <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col">
+      <!-- Navbar -->
+      <div class="w-full navbar bg-base-300 lg:hidden">
+        <div class="flex-none lg:hidden">
+          <label for="my-drawer-3" class="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-6 h-6 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </label>
+        </div>
+      </div>
+      <!-- Page content here -->
       <WelcomeKitchenSink />
     </div>
-    <RouterView />
-  </main>
-
+    <div class="drawer-side">
+      <label for="my-drawer-3" class="drawer-overlay"></label>
+      <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+        <!-- Sidebar content here -->
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
+    </div>
+  </div>
 </template>
