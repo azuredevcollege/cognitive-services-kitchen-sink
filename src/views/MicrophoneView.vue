34deded149df86dde41f1898c14a00f12e45c3e8
Sophia Cullen <socullen@microsoft.com>
2022-05-10 11:30:32 +0200

    Add microphone capabilites MicrophoneView in speech section

--- src/views/MicrophoneView.vue
+++ src/views/MicrophoneView.vue
@@ -1,4 +1,50 @@
+<script setup lang="ts">
+import { onMounted, reactive } from "vue";
+
+import {
+  SpeechConfig,
+  AudioConfig,
+  SpeechRecognizer,
+  SpeechRecognitionEventArgs,
+} from "microsoft-cognitiveservices-speech-sdk";
+
+onMounted(() => {
+  console.log(`the component is now mounted.`);
+
+  navigator.mediaDevices
+    .getUserMedia({ audio: true, video: false })
+    .then(onStream);
+});
+
+const state = reactive({ text: "" });
+var recognizer: SpeechRecognizer;
+var selectedLanguage = "de-DE";
+const speechApiKey = "6a7021f06ec949d7a650ea641f06b258";
+const region = "westeurope";
+function onStream(stream: MediaStream) {
+  const speechConfig = SpeechConfig.fromSubscription(speechApiKey, region);
+  speechConfig.speechRecognitionLanguage = selectedLanguage;
+  const audioConfig = AudioConfig.fromStreamInput(stream);
+  recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+  recognizer.recognizing = onRegonitionResult;
+  recognizer.recognized = onRegonitionResult;
+  recognizer.startContinuousRecognitionAsync();
+}
+function onRegonitionResult(
+  sender: any,
+  event: SpeechRecognitionEventArgs
+): void {
+  state.text = event.result.text;
+}
+function onResult(): void {
+  recognizer.stopContinuousRecognitionAsync();
+}
+function onChange(e: any) {
+  selectedLanguage = e.target.value;
+  console.log(e.target.value);
+}
+</script>
+
 <template>
-  <!-- <VueRecordAudio /> -->
-  Hier kommt das Mikrofon hin
+  {{ state.text }}
 </template>
