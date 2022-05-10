0a0d2959284a3cd839c9c8cdfe2396f4fb7c4efa
Clemens Wältken <clemens.waltken@microsoft.com>
2022-05-10 14:43:25 +0200

    Revert "Set alias for microsoft-cognitiveservices-speech-sdk"
    This reverts commit 0e3407a2692200c71035cd2701c4ad4a2aeff486.

--- vite.config.ts
+++ vite.config.ts
@@ -9,17 +9,8 @@ export default defineConfig({
   base: process.env.BASE_PATH ? `/${process.env.BASE_PATH}` : "/",
   plugins: [vue(), vueJsx()],
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "microsoft-cognitiveservices-speech-sdk",
-        replacement: require.resolve(
-          "microsoft-cognitiveservices-speech-sdk/distrib/browser/microsoft.cognitiveservices.speech.sdk.bundle.js"
-        ),
-      },
-    ],
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
 });
