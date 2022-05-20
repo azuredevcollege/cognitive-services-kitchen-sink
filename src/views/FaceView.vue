3b75950658c3a0ce2a69c52955de9c48efd61723
Sophia Cullen <socullen@microsoft.com>
2022-05-20 13:01:46 +0200

    Enable image capture of video stream

--- src/views/FaceView.vue
+++ src/views/FaceView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { reactive } from "vue";
+import { FaceClient } from "@azure/cognitiveservices-face";
+import { ApiKeyCredentials } from "@azure/ms-rest-js";
 
 function startRecording() {
   console.log("Video recording started...");
@@ -11,6 +13,7 @@ function startRecording() {
 const state = reactive({
   stream: new MediaStream(),
   playing: false,
+  image: "",
 });
 
 function stopRecording() {
@@ -21,6 +24,25 @@ function stopRecording() {
 function onStream(stream: MediaStream) {
   state.stream = stream;
   state.playing = true;
+}
+
+function takePicture() {
+  console.log("Trying to take picture...");
+  const track = state.stream.getVideoTracks()[0];
+  let imageCapture = new ImageCapture(track);
+  imageCapture.takePhoto().then(usePicture);
+}
+
+function usePicture(blob: Blob) {
+  state.image = URL.createObjectURL(blob);
+  const credentials = new ApiKeyCredentials({
+    inHeader: { "Ocp-Apim-Subscription-Key": "faceApiKey" },
+  });
+  let faceClient = new FaceClient(credentials, "endpoint");
+  faceClient.face.detectWithStream(blob).then((response) => {
+    console.log("Response from Face API...");
+    console.log(response);
+  });
 }
 </script>
 
@@ -33,6 +55,16 @@ function onStream(stream: MediaStream) {
     <font-awesome-icon icon="camera" />
     Start Video Recording
   </button>
-  <video autoplay :srcObject="state.stream" type="video/mp4"></video>
+  <video
+    autoplay
+    :srcObject="state.stream"
+    type="video/mp4"
+    v-if="state.playing"
+  ></video>
+  <button class="btn gap-2" @click="takePicture" v-if="state.playing">
+    <font-awesome-icon icon="camera" />
+    Take picture
+  </button>
+  <img :src="state.image" />
   To dos: Add Face API - Features: age, face features, glasses
 </template>
