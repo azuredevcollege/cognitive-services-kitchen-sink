a64912e0c57501159ff5203324f7ecf8c8509659
Sophia Cullen <socullen@microsoft.com>
2022-06-09 14:34:49 +0200

    Hide unused drawers

--- src/components/NavbarDrawer.vue
+++ src/components/NavbarDrawer.vue
@@ -8,13 +8,13 @@ import { RouterLink } from "vue-router";
     <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
       <!-- Sidebar content here -->
       <li><RouterLink to="/">Home</RouterLink></li>
-      <li><RouterLink to="/about">About</RouterLink></li>
+      <!-- <li><RouterLink to="/about">About</RouterLink></li> -->
       <li><RouterLink to="/speech">Speech</RouterLink></li>
       <li><RouterLink to="/face">Face</RouterLink></li>
       <li><RouterLink to="/language">Language</RouterLink></li>
-      <li><RouterLink to="/formrecognizer">Form Recognizer</RouterLink></li>
+      <!-- <li><RouterLink to="/formrecognizer">Form Recognizer</RouterLink></li> -->
       <li><RouterLink to="/customvision">Custom Vision</RouterLink></li>
-      <li><RouterLink to="/computervision">Computer Vision</RouterLink></li>
+      <!-- <li><RouterLink to="/computervision">Computer Vision</RouterLink></li> -->
       <li><RouterLink to="/settings">Settings</RouterLink></li>
     </ul>
   </div>
