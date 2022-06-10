bc6ea508c52c3a36ae1aeb828c7e4de79b57c8e6
alschroe <heinz_metzing@web.de>
2022-06-10 14:37:53 +0200

    added sentiment tab

--- src/components/NavbarDrawer.vue
+++ src/components/NavbarDrawer.vue
@@ -11,6 +11,7 @@ import { RouterLink } from "vue-router";
       <!-- <li><RouterLink to="/about">About</RouterLink></li> -->
       <li><RouterLink to="/speech">Speech</RouterLink></li>
       <li><RouterLink to="/face">Face</RouterLink></li>
+      <li><RouterLink to="/translate">Translator</RouterLink></li>
       <li><RouterLink to="/language">Language</RouterLink></li>
       <!-- <li><RouterLink to="/formrecognizer">Form Recognizer</RouterLink></li> -->
       <li><RouterLink to="/customvision">Custom Vision</RouterLink></li>
