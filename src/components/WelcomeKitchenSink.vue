267eb5b1dcc93237e821a7e4d846d81a247c0931
tamarafmsft <tamaraf@microsoft.com>
2022-12-08 18:50:24 +0100

    Added some beauty

--- src/components/WelcomeKitchenSink.vue
+++ src/components/WelcomeKitchenSink.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="greetings">
+  <!-- <div class="greetings">
     <h1 class="blue">Azure Cognitive Services - Kitchen Sink</h1>
     <h3 class="blue">
       Welcome to the Azure Cognitive Services - Kitchen Sink Application. Here,
       you can try out different cognitive services of the Azure Cognitive
       Services stack.
     </h3>
+  </div> --> 
+
+  <div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Azure Cognitive Services - Kitchen Sink</h1>
+      <p class="py-6">Welcome to the Azure Cognitive Services - Kitchen Sink Application. Here,
+      you can try out different cognitive services of the Azure Cognitive
+      Services stack.</p>
+    </div>
   </div>
+</div>
+
 </template>
 
 <style scoped>
