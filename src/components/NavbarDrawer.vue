2f24c8179cbd05d9325181ad598eeed10243ebc7
tamarafmsft <tamaraf@microsoft.com>
2022-12-08 18:49:02 +0100

    Added some beauty

--- src/components/NavbarDrawer.vue
+++ src/components/NavbarDrawer.vue
@@ -3,9 +3,17 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
+
+<div class="drawer drawer-mobile">
+  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col items-center justify-center">
+    <!-- Page content here -->
+    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+  
+  </div> 
   <div class="drawer-side">
-    <label for="drawer" class="drawer-overlay"></label>
-    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+    <label for="my-drawer-2" class="drawer-overlay"></label> 
+    <ul class="menu p-4 w-80 bg-base-100 text-base-content font-bold">
       <!-- Sidebar content here -->
       <li><RouterLink to="/">Home</RouterLink></li>
       <!-- <li><RouterLink to="/about">About</RouterLink></li> -->
@@ -17,6 +25,8 @@ import { RouterLink } from "vue-router";
       <li><RouterLink to="/customvision">Custom Vision</RouterLink></li>
       <!-- <li><RouterLink to="/computervision">Computer Vision</RouterLink></li> -->
       <li><RouterLink to="/settings">Settings</RouterLink></li>
+      
     </ul>
   </div>
+</div>
 </template>
