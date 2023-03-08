b94987d364b345ac6071d3de15f26c03945ea585
alschroe <heinz_metzing@web.de>
2023-03-08 15:34:31 +0100

    openai18

--- src/views/OpenAIView.vue
+++ src/views/OpenAIView.vue
@@ -20,7 +20,7 @@ function deployments(){
       'api-key': settings.openaikey
     }
   }).then(function(response){
-    console.log(response.data[1].id);
+    console.log(JSON.stringify(response.data[1].id));
     for (let i=0; i<response.data.length; i++){
       modeloptions.push(JSON.stringify(response.data[i].id))
       console.log(i);
